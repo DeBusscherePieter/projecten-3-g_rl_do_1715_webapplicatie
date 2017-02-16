6b0f744110e136c601e09a813f6b89951b2ff2aa
DeBusscherePieter <pjdbussc@gmail.com>
2017-02-16 11:09:52 +0100

    Sendgrid integration

--- app.js
+++ app.js
@@ -301,14 +301,17 @@ app.put('/activiteitenList/:id', function(req, res){
 var generatePassword = require('password-generator');
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-//var transporter = nodemailer.createTransport('smtps://hogent.mijnresto@gmail.com:MijnRest0@smtp.gmail.com');
-var transporter = nodemailer.createTransport(smtpTransport({
-   service: 'Gmail',
-   auth: {
-       user: 'hogent.mijnresto@gmail.com',
-       pass: 'MijnRest0'
-   }
-}));
+
+var sgTransport = require('nodemailer-sendgrid-transport');
+
+var transporter = nodemailer.createTransport({
+  service: 'SendGrid',
+  auth: {
+    user: 'MijnResto',
+    pass: 'MijnRest0'
+  }
+});
+
 //var db4 = mongojs('loginapp', ['users']);
 app.post('/users/vergeten', function(req, res){
   var gebruiker = req.body.username;
